350ed7e5f232c3cbfd11e03fceb6429e62008622
kaito-a-bit <arakai0128@gmail.com>
2021-10-20 23:59:38 +0900

    Started this app project.

---
+++ YouDoYouBetter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouDoYouBetter
+//
+//  Created by 荒井海斗 on 2021/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
